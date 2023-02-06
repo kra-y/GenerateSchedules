47e5ae13264548d11abe76ee8e580ae080c83546
kra-y <12517923+kra-y@users.noreply.github.com>
2023-02-06 08:53:01 -0700

    why do none of these functions work anymore?

--- CourseCatalog.R
+++ CourseCatalog.R
@@ -1,11 +1,16 @@
 #Make Course Catalog
 library(stats)
-numbers = c(980,1010,1200,1220,1230,1260)
+numbers = c(0980,1010,1200,1220,1230,1260)
 probs0<-rexp(6,1)
 probs<-probs0/sum(probs0)
 mathprobs<-sort(probs,decreasing = T)
 course_names<-paste("MATH",numbers,sep = "")
 
-math_students<-sample(course_names,100,replace = T,prob = c(mathprobs))
-x<-as.factor(math_students)
-plot(x)
+math_enrollments<-sample(course_names,2000,replace = T,prob = mathprobs)
+
+
+student_ID<-paste0("S",1000:2999)
+
+
+math<-data.frame("STUDENT_ID" = student_ID,
+                 "COURSE" = math_enrollments)
